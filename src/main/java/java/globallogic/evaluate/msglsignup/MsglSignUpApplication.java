dd20b56cd8d313502cc9038d8ea34453c1751ced
Grishka <ignacioencizo@gmail.com>
2022-07-20 18:29:21 -0300

    Initial commit

---
+++ src/main/java/java/globallogic/evaluate/msglsignup/MsglSignUpApplication.java
@@ -0,0 +1,13 @@
+package java.globallogic.evaluate.msglsignup;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsglSignUpApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MsglSignUpApplication.class, args);
+	}
+
+}
---
+++ src/test/java/java/globallogic/evaluate/msglsignup/MsglSignUpApplicationTests.java
@@ -0,0 +1,13 @@
+package java.globallogic.evaluate.msglsignup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsglSignUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
